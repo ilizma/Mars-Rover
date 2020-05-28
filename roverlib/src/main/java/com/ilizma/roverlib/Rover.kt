44e00457c5461428a0e484c42b9ad7269cb5796b
Iosu <lizarraga.iosu@gmail.com>
2020-05-28 13:20:40 +0200

    [WIP] add internal Rover and DataJson classes

---
+++ roverlib/src/main/java/com/ilizma/roverlib/Rover.kt
@@ -0,0 +1,72 @@
+package com.ilizma.roverlib
+
+import com.ilizma.roverlib.entity.DataJson
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+private const val NORTH = "N"
+private const val EAST = "E"
+private const val SOUTH = "S"
+private const val WEST = "W"
+private const val LEFT = "L"
+private const val RIGHT = "R"
+private const val MOVE = "M"
+
+internal class Rover {
+
+    internal fun move(json: String): String {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        try {
+            val data = moshi.adapter(DataJson::class.java).fromJson(json)
+            data?.let { safeData ->
+                val topRightCornerX = safeData.topRightCorner.xCoordinate
+                val topRightCornerY = safeData.topRightCorner.yCoordinate
+                var roverPositionX = safeData.roverPosition.xCoordinate
+                var roverPositionY = safeData.roverPosition.yCoordinate
+                var direction = safeData.roverDirection
+                val movements = safeData.movements.toList().map { it.toString() }
+
+                movements.forEach { movement ->
+                    when (movement) {
+                        LEFT -> direction = spinLeft(direction)
+                        RIGHT -> direction = spinRight(direction)
+                        MOVE -> when (direction) {
+                            NORTH -> if (roverPositionY < topRightCornerY) roverPositionY++
+                            EAST -> if (roverPositionX < topRightCornerX) roverPositionX++
+                            SOUTH -> if (roverPositionY > 0) roverPositionY--
+                            WEST -> if (roverPositionX > 0) roverPositionX--
+                        }
+                        else -> throw Exception("Incorrect movement, only L R M accepted")
+                    }
+                }
+                return "$roverPositionX $roverPositionY $direction"
+            } ?: throw Exception("No data entered")
+        } catch (e: Exception) {
+            throw Exception("${DataJson::class.java.simpleName} parse failed: $e")
+        }
+    }
+
+    private fun spinLeft(direction: String): String {
+        return when (direction) {
+            NORTH -> WEST
+            EAST -> NORTH
+            SOUTH -> EAST
+            WEST -> SOUTH
+            else -> throw Exception("Incorrect direction, only N E S W accepted")
+        }
+    }
+
+    private fun spinRight(direction: String): String {
+        return when (direction) {
+            NORTH -> EAST
+            EAST -> SOUTH
+            SOUTH -> WEST
+            WEST -> NORTH
+            else -> throw Exception("Incorrect direction, only N E S W accepted")
+        }
+    }
+
+}
