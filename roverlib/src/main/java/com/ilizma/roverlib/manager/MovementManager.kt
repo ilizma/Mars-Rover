330b3e7146ea811f1a488c5f18321adb235d8fc2
Iosu Lizarraga <lizarraga.iosu@gmail.com>
2020-06-01 12:53:34 +0200

    Roverlib kdoc (#8)
    * [FEAT] add kdoc to roverlib classes
    
    * [FEAT] add readme to roverlib

--- roverlib/src/main/java/com/ilizma/roverlib/manager/MovementManager.kt
+++ roverlib/src/main/java/com/ilizma/roverlib/manager/MovementManager.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Iosu Lizarraga Madinabeitia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ilizma.roverlib.manager
 
 import com.ilizma.roverlib.base.IncorrectDirection
@@ -15,6 +31,11 @@ private const val LEFT = "L"
 private const val RIGHT = "R"
 private const val MOVE = "M"
 
+/**
+ * internal MovementManager class
+ *
+ * @property jsonAdapter the jsonAdapter<DataJson> to get the DataJson object from a json
+ */
 internal class MovementManager(private val jsonAdapter: JsonAdapter<DataJson>) {
 
     private var topRightCornerX: Int = 0
@@ -24,6 +45,28 @@ internal class MovementManager(private val jsonAdapter: JsonAdapter<DataJson>) {
     private lateinit var direction: String
     private lateinit var movements: List<String>
 
+    /**
+     * moves Rover on the plateau
+     *
+     * Example of the input json and output String:
+     * input: {
+     *     topRightCorner: {
+     *         x: 5,
+     *         y: 5
+     *     },
+     *     roverPosition: {
+     *         x: 1,
+     *         y: 2
+     *     },
+     *     roverDirection: "N",
+     *     movements: "LMLMLMLMM"
+     * }
+     * output: "1 3 N"
+     *
+     * @property json the json that contains the information of the topRightCorner, roverPosition,
+     * roverDirection and the movements
+     * @return the new position of Rover after do the movements
+     */
     fun move(json: String): String {
         try {
             val data = jsonAdapter.fromJson(json)
